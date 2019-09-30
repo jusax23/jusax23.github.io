50673551aafcb27cbdf34d7e9f2b472bff699ac6
jusax23 <jusax23@gmail.com>
2019-09-30 21:31:49 +0200

    Update index.js

--- findit/index.js
+++ findit/index.js
@@ -1,17 +1,38 @@
 
 function onload(){
   getLocation();
-  x = document.getElementById("canvas");
+  window.addEventListener('deviceorientation', function(eventData) {
+    var tiltLR = eventData.gamma;
+    var tiltFB = eventData.beta;
+    var idir = eventData.alpha
+    cam.rot[1] = idir;
+    cam.rot[0] = tiltFB;
+  }, false);
+  c = document.getElementById("canvas");
+  ctx = c.getContext("2d");
+  render = new Render(ctx,obj,cam);
 }
-var x;
+
+var cam = {pos:[0,0,0],rot:[0,0]};
+var c;
+var ctx;
+var render;
+var obj = [{pos:[-9,0,5],size:[1,1,1],rot:[0,0]}];
+
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.watchPosition(showPosition);
   } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
+
   }
 }
 function showPosition(position) {
+  cam.pos[2] = position.coords.longitude;
+  cam.pos[0] = position.coords.latitude;
+  cam.pos[1] = position.coords.altitude;
+}
 
-  x.innerHTML = "Latitude: " + position.coords.latitude +  "<br>Longitude: " + position.coords.longitude;
+
+function tick(){
+  window.requestAnimationFrame(tick);
 }
