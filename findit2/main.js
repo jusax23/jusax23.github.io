7d44669e238b6420ff25be9ac0f6f60dc8104a5d
jusax23 <jusax23@gmail.com>
2019-10-19 17:53:38 +0200

    Update main.js

--- findit2/main.js
+++ findit2/main.js
@@ -1,9 +1,26 @@
 function onload(){
 
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  video = document.getElementById('video');
+  if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true }).then(function(stream) {
+        //video.src = window.URL.createObjectURL(stream);
+        video.srcObject = stream;
+        video.play();
+    });
+  }
 }
 
+const canvas;
+const ctx;
+const video;
 
 function tick(){
+  var my = canvas.height;
+  var mx = canvas.width;
+
   window.requestAnimationFrame(tick);
   console.log("test");
+  context.drawImage(video, 0, 0, mx, my);
 }
